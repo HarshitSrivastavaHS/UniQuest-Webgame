26347e9513ddd65c0b7f82cfbf44c70284e5d1b5
Harshit Srivastava <harshitsrivastava7149@gmail.com>
2025-06-28 19:34:46 +0530

    Milestone 2 last min fix

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751118430|27487794748';
+const CACHE_VERSION = '1751119461|28518972943';
 /** @type {string} */
 const CACHE_PREFIX = 'UniQuest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
