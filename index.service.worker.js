f87bd12c08b58fa35e367b9e8c4b1150a9f1bacf
Harshit Srivastava <harshitsrivastava7149@gmail.com>
2025-07-28 10:41:18 +0530

    Milestone3 game

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753613010|4059348207';
+const CACHE_VERSION = '1753679008|32460300';
 /** @type {string} */
 const CACHE_PREFIX = 'UniQuest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
